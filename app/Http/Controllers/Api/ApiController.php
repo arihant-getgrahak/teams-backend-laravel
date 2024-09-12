13bd1728363760c16f45d79b0bc3d3fa6a6dd2f8
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-12 16:23:42 +0530

    feat: 1. install the api.php for routes 2. databse connection 3. JWT installation 4. Model Settings 5. API routes setup 6. Create API's for register, login, profile, refresh token, logout

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,36 +5,97 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth; 
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
     //Register API (POST, Formdata)
-    pulic function register()
+    public function register(Request $request)
     {
+        //Data validation
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
+        ]);
 
+        // User Model
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+
+        // Response
+        return response()->json([
+            "status" => true,
+            "message" => "User registered successfully"
+        ]);
     }
 
     //Login API (POST, Formdata)
-    public function login()
+    public function login(Request $request)
     {
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
 
+        // JWTAuth
+        $token = JWTAuth::attempt([
+            "email" => $request->email,
+            "password" => $request->password
+        ]);
+
+        if(!empty($token)){
+
+            return response()->json([
+                "status" => true,
+                "message" => "User logged in succcessfully",
+                "token" => $token
+            ]);
+        }
+
+        // Response
+        return response()->json([
+            "status" => false,
+            "message" => "Invalid details"
+        ]);
     }
 
     //Profile API (GET, autherization token value JWT)
     public function profile()
     {
+        $userdata = auth()->user();
 
+        return response()->json([
+            "status" => true,
+            "message" => "Profile data",
+            "data" => $userdata,
+        ]);
     }
 
     //Refresh token API(GET)
     public function refreshToken()
     {
+        $newToken = auth()->refresh();
 
+        return response()->json([
+            "status" => true,
+            "message" => "Token refreshed",
+            "token" => $newToken
+        ]);
     }
 
     //Logout API(GET)
     public function logout()
     {
+        auth()->logout();
 
+        return response()->json([
+            "status" => true,
+            "message" => "User logged out successfully"
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Auth\Middleware\Authenticate;
 use App\Http\Controllers\Api\ApiController;
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
-// API Routes
 Route::post("register", [ApiController::class, "register"]);
 Route::post("login", [ApiController::class, "login"]);
 
@@ -17,7 +16,7 @@ Route::group([
     "middleware" => ["auth:api"]
 ], function(){
 
-    Route::get("prfile", [ApiController::class, "profile"]);
+    Route::get("profile", [ApiController::class, "profile"]);
     Route::get("refresh", [ApiController::class, "refreshToken"]);
     Route::get("logout", [ApiController::class, "logout"]);
 });
