0d436ca5cad357989c43e4b4c1d9fb9ed197c790
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-18 11:34:59 +0530

    feat: create group database

--- database/migrations/2024_09_18_054756_create_groups_table.php
+++ database/migrations/2024_09_18_054756_create_groups_table.php
@@ -4,15 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('groups', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->uuid("sender_id");
+            $table->uuid("receiver_id");
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+            $table->json("receiver_id")->nullable();
+            $table->text("message");
+            $table->boolean("isUpdate")->default(false);
+            $table->boolean('isDelete')->default(false);
+            $table->dateTime("deletedAt")->nullable();
             $table->timestamps();
         });
     }
