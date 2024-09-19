676c85833e38f06038ce8efc602214be6dbc2e0d
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-19 14:35:46 +0530

    create organization model

---
+++ app/Models/Organization.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Str;
+class Organization extends Model
+{
+    use HasFactory;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        "name",
+        "created_by",
+        "description",
+        "group_id",
+        "user_id",
+    ];
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'organizations');
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, "created_by");
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+}
