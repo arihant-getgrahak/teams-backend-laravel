733f462e1bbb4a881265880ff94e771a71b0967c
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-20 18:50:37 +0530

    merge organization_feature branch into invite_feature

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InviteController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\UserController;
