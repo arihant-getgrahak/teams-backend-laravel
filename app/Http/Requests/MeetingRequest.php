6c1eee1f6ed8cb1e33e3b8e330a64a37084f7371
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-24 18:51:22 +0530

    update MettingRequest

--- app/Http/Requests/MeetingRequest.php
+++ app/Http/Requests/MeetingRequest.php
@@ -11,7 +11,7 @@ class MeetingRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        if(auth()->check()) {
+        if (auth()->check()) {
             return true;
         }
         return false;
@@ -26,19 +26,19 @@ class MeetingRequest extends FormRequest
     {
         return [
             'group_id' => 'required|exists:groups,id',
-            'scheduled_at'=> 'required|date',
-            'title'=> 'required|string|max:255',
+            'scheduled_at' => 'required|date',
+            'title' => 'required|string|max:255',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'group_id.required' => 'Group is required',
-            'group_id.exists' => 'Group does not exist',
-            'scheduled_at.required' => 'Scheduled time is required',
-            'agenda.required' => 'Agenda is required',
-            'agenda.max' => 'Agenda is too long',
+            'group_id.required' => __("validation.required", ["attribute" => "समूह"]),
+            'group_id.exists' => __('validation.exists', ['attribute' => 'समूह']),
+            'scheduled_at.required' => __('validation.required', ['attribute' => 'समय']),
+            'agenda.required' => __('validation.required', ['attribute' => 'अध्ययन']),
+            'agenda.max' => __('validation.max', ['attribute' => 'अध्ययन', 'max' => 255]),
         ];
     }
 }
