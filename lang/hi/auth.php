2f7cdeb328228ca7eaec90d1d5f47535fb80a577
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-24 20:31:26 +0530

    create lang files

---
+++ lang/hi/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'ये प्रमाण-पत्र हमारे रिकॉर्ड से मेल नहीं खाते|',
+    'password' => 'दिया गया पासवर्ड ग़लत है|',
+    'throttle' => 'बहुत अधिक लॉगिन प्रयास. कृपया :सेकंड सेकंड में पुनः प्रयास करें।',
+    'setlanguage' => 'भाषा को सेट कर दिया गया है',
+    'register' => 'उपयोगकर्ता सफलतापूर्वक पंजीकृत हो गया है',
+    'login' => 'उपयोगकर्ता सफलतापूर्वक लॉग इन हुआ',
+    'profile' => 'उपयोगकर्ता प्रोफाइल',
+    'logout' => 'उपयोगकर्ता सफलतापूर्वक लॉग आउट हो गया है',
+    'setuserlanguage' => 'उपयोगकर्ता भाषा को अंग्रेजी(en) या हिंदी(hi) में सेट करना चाहिए',
+
+];
