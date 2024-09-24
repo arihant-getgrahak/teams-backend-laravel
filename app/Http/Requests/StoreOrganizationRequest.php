7efef56d7a3e479e932132673648fdbb93557193
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-24 19:00:40 +0530

    update StoreOrganizationRequest

--- app/Http/Requests/StoreOrganizationRequest.php
+++ app/Http/Requests/StoreOrganizationRequest.php
@@ -11,11 +11,11 @@ class StoreOrganizationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        if(auth()->check()) {
+        if (auth()->check()) {
             return true;
         }
         return false;
-    
+
     }
 
     /**
@@ -27,16 +27,16 @@ class StoreOrganizationRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'description'=> 'nullable|string',
+            'description' => 'nullable|string',
         ];
     }
     public function messages(): array
     {
         return [
-            'name.required'=> 'name is required',
-            'name.string'=> 'name must be string',
-            'name.max' => 'The name may not be greater than 255 characters.',
-            'description.string'=> 'description must be string',
+            'name.required' => __('validation.required', ["attribute" => "नाम"]),
+            'name.string' => __('validation.string', ["attribute" => "नाम"]),
+            'name.max' => __('validation.max', ["attribute" => "नाम", "max" => 255]),
+            'description.string' => __('validation.string', ["attribute" => "विवरण"]),
         ];
     }
 }
