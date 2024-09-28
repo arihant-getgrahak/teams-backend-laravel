b983bb80da7ac5da0f1a11b314780f0ad8d2ef95
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-28 23:13:42 +0530

    add lang translation for hi

---
+++ lang/hi/translation.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    "onboarding" => [
+        "auth" => [
+            "register" => [
+                "title" => "पंजीकरण करें",
+                "name_placeholder" => "अपना नाम दर्ज करें",
+                "email_placeholder" => "अपना ईमेल दर्ज करें",
+                "password_placeholder" => "अपना पासवर्ड दर्ज करें",
+                "confirm_password_placeholder" => "अपना पासवर्ड पुष्टि करें",
+                "designation_placeholder" => "अपना पदनाम दर्ज करें",
+                "already_have_account" => "पहले से ही एक खाता है?",
+                "login_button_text" => "लॉगिन करें",
+                "submit_button_text" => "पंजीकरण करें",
+                "create_account_button_text" => "खाता बनाएँ"
+            ],
+            "login" => [
+                "title" => "लॉगिन करें",
+                "email_placeholder" => "अपना ईमेल दर्ज करें",
+                "password_placeholder" => "अपना पासवर्ड दर्ज करें",
+                "submit_button_text" => "लॉगिन करें",
+                "dont_have_account" => "खाता नहीं है?",
+                "register_button_text" => "पंजीकरण करें"
+            ]
+        ],
+    ],
+    "homeScreen" => [
+        "title" => "होम",
+        "options" => [
+            "profile" => "प्रोफ़ाइल",
+            "new_chat" => "नया चैट",
+            "search" => "खोजें",
+            "schedule" => "अनुसूची",
+            "join_chat" => "चैट में शामिल हों",
+            "search_user" => "उपयोगकर्ता खोजें"
+        ]
+    ],
+    "newChat" => [
+        "title" => "नया चैट",
+        "start_new_chat" => "एक नया चैट शुरू करें",
+        "username_placeholder" => "उपयोगकर्ता नाम दर्ज करें",
+        "receiver_id_placeholder" => "प्राप्तकर्ता आईडी दर्ज करें",
+        "start_chat_button" => "चैट शुरू करें",
+        "search_user_button" => "उपयोगकर्ता खोजें",
+        "no_user_found" => "कोई उपयोगकर्ता नहीं मिला",
+        "search_for_user" => "उपयोगकर्ता की खोज करें"
+    ],
+    "chatScreen" => [
+        "title" => "चैट स्क्रीन",
+        "type_here_placeholder" => "यहाँ संदेश लिखें"
+    ],
+    "profile" => [
+        "title" => "प्रोफ़ाइल",
+        "name_placeholder" => "अपना नाम दर्ज करें",
+        "email_placeholder" => "अपना ईमेल दर्ज करें",
+        "designation_placeholder" => "अपना पदनाम दर्ज करें"
+    ]
+];
