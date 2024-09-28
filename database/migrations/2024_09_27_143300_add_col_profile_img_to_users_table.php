b30f96cdfc0666f912332d24669a2ba973037ba1
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-28 23:25:24 +0530

    change the column name

--- database/migrations/2024_09_27_143300_add_col_profile_img_to_users_table.php
+++ database/migrations/2024_09_27_143300_add_col_profile_img_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_img')->nullable();
+            $table->string('profile_image')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('profile_img');
+            $table->dropColumn('profile_image');
         });
     }
 };
