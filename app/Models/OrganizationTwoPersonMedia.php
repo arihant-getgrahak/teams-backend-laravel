c94f1ac0f1dab2d83cefe267806aab2623541691
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-30 20:29:02 +0530

    create the model for two person media

---
+++ app/Models/OrganizationTwoPersonMedia.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Str;
+class OrganizationTwoPersonMedia extends Model
+{
+    use HasFactory;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'filename',
+        'file_path',
+        'sender_id',
+        'receiver_id',
+        'organization_id',
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, "sender_id");
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, "receiver_id");
+    }
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class, "organization_id");
+    }
+}
