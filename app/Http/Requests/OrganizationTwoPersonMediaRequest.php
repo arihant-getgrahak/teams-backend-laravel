567e2e07f1983755ac010dabeca6ea6415e40555
sonaljain01 <jainsonal8630407466@gmail.com>
2024-09-30 20:29:27 +0530

    create the organization two person media request file

---
+++ app/Http/Requests/OrganizationTwoPersonMediaRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrganizationTwoPersonMediaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if (auth()->check()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'files' => 'required|array',
+            'files.*' => 'file|mimes:jpeg,png,jpg,gif,svg,pdf,doc,docx,zip|max:2048',
+            'receiver_id' => 'required|exists:users,id',
+            'organization_id' => 'required|exists:organizations,id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'file.mimes' => 'The :attribute must be a file of type: jpeg, png, jpg, gif, svg, pdf, doc, docx, zip.',
+            'file.max' => 'The :attribute may not be greater than :max kilobytes.',
+            'organization_id' => 'The :attribute must be a file of type: jpeg, png, jpg, gif, svg, pdf, doc, docx, zip.',
+        ];
+    }
+}
