3ae32d1a43e17aecec7f876f9589b4e93b96363d
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-01 16:35:16 +0530

    update audit functionality in model

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Str;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Meeting extends Model
+
+
+class Meeting extends Model implements Auditable
 {
     use HasFactory;
+
+    use \OwenIt\Auditing\Auditable;
     public static function boot()
     {
         parent::boot();
